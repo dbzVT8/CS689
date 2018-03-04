90b40a3c2af61329b072e4d6d44b2aaef48b165c
Suraj Kulkarni <sur2124@gmail.com>
2018-03-04 11:12:07 -0500

    refactoring code

--- Project2PFP/CppManipPFP/src/ManipPlanner.cpp
+++ Project2PFP/CppManipPFP/src/ManipPlanner.cpp
@@ -1,4 +1,5 @@
 #include "ManipPlanner.hpp"
+#include <vector>
 
 ManipPlanner::ManipPlanner(ManipSimulator * const manipSimulator)
 {
@@ -10,105 +11,81 @@ ManipPlanner::~ManipPlanner(void)
     //do not delete m_simulator  
 }
 
-
-double getDerivativeShortCut( int j, int i,ManipSimulator * const manipSimulator, bool isX )
+std::vector<double> makeUnitVector(std::vector<double> input)
 {
-    if(isX)
+    double magnitude = 0;
+    std::vector<double> output(input.size());
+    for(int i = 0; i < input.size(); i++)
     {
-        return ((manipSimulator ->GetLinkEndY(j)) * -1) + (manipSimulator ->GetLinkStartY(i));
+        magnitude = magnitude + pow(input[i], 2);
     }
-    else
+    magnitude = sqrt(magnitude);
+
+    for(int i = 0; i < input.size(); i++)
     {
-        return (manipSimulator ->GetLinkEndX(j)) - (manipSimulator ->GetLinkStartX(i));
+        output[i] = input[i] / magnitude;
     }
+    return output;
 }
 
-Point getLocalPoint(Point origin, double x, double y)
+std::vector<double> getRepulsiveForce(double xJoint, double yJoint, int numberOfObstacles, ManipSimulator * const manipSimulator)
 {
-    Point local;
-    local.m_x = x - origin.m_x;
-    local.m_y = y - origin.m_y;
-    return local;
-}
-
-Point getRepulsiveForce(double xJoint, double yJoint, int numberOfObstacles, ManipSimulator * const manipSimulator)
-{
-    Point sumOfRep;
-    sumOfRep.m_x = 0;
-    sumOfRep.m_y = 0;
+    std::vector<double> sumOfRep(2);
     for(int i = 0; i < numberOfObstacles; i = i+1)
     {
         Point p = manipSimulator ->ClosestPointOnObstacle(i, xJoint, yJoint);
         double x = xJoint - p.m_x;
         double y = yJoint - p.m_y;
-        sumOfRep.m_x = sumOfRep.m_x + x;
-        sumOfRep.m_y = sumOfRep.m_y + y;
+        sumOfRep[0] = sumOfRep[0] + x;
+        sumOfRep[1] = sumOfRep[1] + y;
     }
     return sumOfRep;
 }
+
 
 void ManipPlanner::ConfigurationMove(double allLinksDeltaTheta[])
 {
     double totalForce = 0;
     double x_goal = m_manipSimulator ->GetGoalCenterX();
     double y_goal = m_manipSimulator ->GetGoalCenterY();
-    int numberOfLinks = m_manipSimulator ->GetNrLinks();
-    int numberOfObstacles = m_manipSimulator ->GetNrObstacles();
-    double U[sizeof(allLinksDeltaTheta)];
-    for(int i = 0; i < numberOfLinks; i++)
-    {
-        U[i] = 0; 
-    }
+    const int numberOfLinks = m_manipSimulator ->GetNrLinks();
+    const int numberOfObstacles = m_manipSimulator ->GetNrObstacles();
+    std::vector<double> U(numberOfLinks);
+    
     for(int j = 0; j < numberOfLinks; j = j+1)
     {
-        Point forceAtIndex;
-        forceAtIndex.m_x = 0;
-        forceAtIndex.m_y = 0;
         double x_joint = m_manipSimulator ->GetLinkEndX(j);
         double y_joint = m_manipSimulator ->GetLinkEndY(j);
+        
+        std::vector<double> force(2);
         if(j == numberOfLinks - 1)
         {
             double attractiveX = x_goal - x_joint;
             double attractiveY = y_goal - y_joint;
-            Point repulsiveForce = getRepulsiveForce(x_joint, y_joint, numberOfObstacles, m_manipSimulator);
-            forceAtIndex.m_x = repulsiveForce.m_x + attractiveX;
-            forceAtIndex.m_y = repulsiveForce.m_y + attractiveY;
+            std::vector<double> repulsiveForce = getRepulsiveForce(x_joint, y_joint, numberOfObstacles, m_manipSimulator);
+            force[0] = repulsiveForce[0] + attractiveX;
+            force[1] = repulsiveForce[1]  + attractiveY;
         }
         else
         {
-            forceAtIndex = getRepulsiveForce(x_joint, y_joint, numberOfObstacles,  m_manipSimulator);
+             force = getRepulsiveForce(x_joint, y_joint, numberOfObstacles,  m_manipSimulator);
         }
-
-        double force[2];
-        force[0] = forceAtIndex.m_x;
-        force[1] = forceAtIndex.m_y;
 
         for(int i = 0; i < numberOfLinks; i++)
         {
-            double jacobianTransposei0 = getDerivativeShortCut(j, i, m_manipSimulator, true);
-            double jacobianTransposei1 = getDerivativeShortCut(j, i, m_manipSimulator, false);
+            double jacobianTransposei0 = ((m_manipSimulator ->GetLinkEndY(j)) * -1) + (m_manipSimulator ->GetLinkStartY(i));
+            double jacobianTransposei1 = (m_manipSimulator ->GetLinkEndX(j)) - (m_manipSimulator ->GetLinkStartX(i));
             
             U[i] = U[i] + force[0] * jacobianTransposei0 + force[1] * jacobianTransposei1;
         }
 
     }
 
-    //make it a unit vector
-    double magnitude = 0;
-    for(int i = 0; i < numberOfLinks; i++)
-    {
-        magnitude = magnitude + pow(U[i], 2);
-    }
-    magnitude = sqrt(magnitude);
+    U = makeUnitVector(U);
 
     for(int i = 0; i < numberOfLinks; i++)
     {
-        U[i] = U[i] / magnitude;
-    }
-
-    for(int i = 0; i < numberOfLinks; i++)
-    {
-        allLinksDeltaTheta[i] = U[i] * .1;
+        allLinksDeltaTheta[i] = U[i] * .01;
     }
 }
 
