e3923c79b13c04f393f9a331f201264ed36a1c48
Suraj Kulkarni <sur2124@gmail.com>
2018-05-03 21:05:33 -0400

    more useful classes and methods

--- ProjectVREP/Utilities.py
+++ ProjectVREP/Utilities.py
@@ -16,34 +16,71 @@ class SafeInterval(object):
 class Point(object):
     '''Creates a point on a coordinate plane with values x and y.'''
 
-    def __init__(self, x, y, z, isGoal=False, isStart=False):
+    def __init__(self, x, y, z, safeIntervals=[]):
         '''Defines x and y variables'''
-        self.X = x
-        self.Y = y
-        self.Z = z
-        self.isGoal = isGoal
-        self.isStart = isStart
+        self.x = x
+        self.y = y
+        self.z = z
+        self.safeIntervals = safeIntervals
 
     def __str__(self):
-        return "Point(%s,%s,%s)"%(self.X, self.Y, self.Z)
+        return "Point(%s,%s,%s)"%(self.x, self.y, self.z)
 
     def distance(self, other):
-        dx = self.X - other.X
-        dy = self.Y - other.Y
-        dz = self.Z - other.Z
+        dx = self.x - other.X
+        dy = self.y - other.Y
+        dz = self.z - other.Z
         return math.sqrt(dx**2 + dy**2 + dz**2)
 
+    def equals(self, other):
+        return self.distance(other) == 0
+
 class Edge(object):
-    def __init__(self, p1, p2, safeIntervals=[]):
+    def __init__(self, p1, p2):
         '''Defines x and y variables'''
         self.point1 = p1
         self.point2 = p2
-        self.safeIntervals = safeIntervals
 
 
 class Graph(object):
     def __init__(self, edges=[]):
-        self.ListOfEdges = edges
+        self.listOfEdges = edges
+
+    def getNeighborPoints(self, point):
+        neighbors = []
+        for edge in self.getNeighborEdges(point):
+            if edge.point1.equals(point):
+                neighbors.append(edge.point2)
+            elif edge.point2.equals(point):
+                neighbors.append(edge.point1)
+        return neighbors
+
+    def getNeighborEdges(self, point):
+        neighbors = []
+        for edge in self.listOfEdges:
+            if edge.point1.equals(point):
+                neighbors.append(edge)
+            elif edge.point2.equals(point):
+                neighbors.append(edge)
+        return neighbors
+
+
+class Robot(object):
+    def __init__(self, start, goal):
+        self.goal = goal
+        self.currentLocation = start
+
+    def isAtGoal(self):
+        return self.currentLocation.equals(self.goal)
+
+    def updateCurrentLocation(self, point):
+        self.currentLocation = point
+
+
+
+
+
+
 
 
 
